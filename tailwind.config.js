fa529a4e7f3ea753dd516d43e6ddb22355d8b106
FAHMYZAR <dafatrans2016@gmai.com>
2025-11-08 16:24:34 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    container: {
+      center: true,
+      padding: "16px",
+    },
+    extend: {
+      colors: {
+        primary: "#14b8a6",
+        secondary: "#94a3b8",
+        dark: "#0f172a",
+        orange: { 
+          500: "#ffa500",
+        },
+      },
+    },
+    screens: {
+      "2xl": "1320px",
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+    },
+  },
+  plugins: [],
+}
